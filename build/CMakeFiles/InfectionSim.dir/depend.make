256280b5423069fee1e4b667d0a4005babc88ad8
kloks <petrovartem193@gmail.com>
2026-05-01 05:00:04 +0300

    AddStructure

---
+++ build/CMakeFiles/InfectionSim.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for InfectionSim.
+# This may be replaced when dependencies are built.
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"Simulation Started"<<std::endl;
+    return 0;
+}
