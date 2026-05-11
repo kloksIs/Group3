ba42e59f8fed4ed68fd33968a4fcfdbde0f3ee29
Elya Bochkareva <elvirabockareva29@gmail.com>
2026-05-11 20:03:04 +0300

    Update Virus.hpp

--- include/Virus.hpp
+++ include/Virus.hpp
@@ -1,24 +1,46 @@
+/**
+ * @file Virus.hpp
+ * @brief Класс Virus - параметры вируса
+ * @author Артем Петров, Эльвира Бочкарева, Елизавета Цепелева, Вадим Горшков
+ * @version 1.0
+ */
+
 #ifndef VIRUS_HPP
 #define VIRUS_HPP
 
 #include <string>
 #include <nlohmann/json.hpp>
 
+/**
+ * @brief Класс, представляющий вирус
+ * @details Содержит название, скорость заражения,
+ *          инкубационный период и уровень летальности
+ */
 class Virus {
 private:
-    std::string name;
-    double infection_rate; // Заразность чел/день от одного больного
-    int incubation_period; // Инкубация в днях
-    double mortality_rate; // Смертность от 0.0 (0%) до 1.0 (100%)
+    std::string name;           ///< Название вируса
+    double infection_rate;      ///< Скорость заражения (чел/день)
+    int incubation_period;      ///< Инкубационный период (дни)
+    double mortality_rate;      ///< Уровень летальности (0.0-1.0)
 
 public:
-    // Конструктор, принимающий блок данных из JSON
+    /**
+     * @brief Конструктор из JSON
+     * @param data JSON-объект с полями: name, infection_rate, 
+     *             incubation_period, mortality_rate
+     */
     Virus(const nlohmann::json& data);
 
-    // Геттеры (чтобы Simulation могла брать данные)
+    /// @brief Возвращает название вируса
     std::string getName() const { return name; }
+    
+    /// @brief Возвращает скорость заражения
     double getInfectionRate() const { return infection_rate; }
+    
+    /// @brief Возвращает инкубационный период
     int getIncubationPeriod() const { return incubation_period; }
+    
+    /// @brief Возвращает уровень летальности
     double getMortalityRate() const { return mortality_rate; }
 };
 
